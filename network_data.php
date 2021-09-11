975ba5817015fa23f0cfbe45c8fa2cf5ac1f2cf4
Rumblee <51049342+halcrews@users.noreply.github.com>
2021-09-11 18:59:57 +0300

    Update network_data.php

--- network_data.php
+++ network_data.php
@@ -7,11 +7,13 @@ if(!R::testConnection()){
     die('Не удалось подключиться к базе!');
 }
 
-
 $cursor = R::getCursor('SELECT * FROM `pc` INNER JOIN `domains` ON `pc`.`domain_name` = `domains`.`domain_name`;');
 
 while($row = $cursor->getNextItem()){
     $arr[] = $row;
 }
 $json_response = json_encode($arr);
+
+$domains = R::getAll('SELECT `domain_name` FROM `domains` where id>0;');
+$domain = json_encode($domains);
 ?>
